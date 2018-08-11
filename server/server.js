879bc77b8a4603f4e049b24ec3e670d7b71e6044
MrInternauta <feldjesus@gmail.com>
2018-08-11 00:47:21 -0500

    Primera carga

---
+++ server/server.js
@@ -0,0 +1,41 @@
+require('./config/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+    // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function(req, res) {
+    res.json('Get usuario')
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    if (body.nombre == undefined) {
+        res.status(400).json({
+            ok: false,
+            mesaje: 'El nombre es necesario'
+        })
+    } else {
+        res.json({ persona: body })
+    }
+})
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id
+    res.json({
+        id
+    })
+})
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchado en port ' + process.env.PORT)
+})
